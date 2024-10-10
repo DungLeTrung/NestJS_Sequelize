2af59608deacf97c7c0311079108388ecadba385
Le Trung Dung <trungdungle1912@gmail.com>
2024-10-10 11:00:55 +0700

    [Final Project] [Auth Module]Setup OTP 3

--- src/modules/auth/dto/verify-otp.dto.ts
+++ src/modules/auth/dto/verify-otp.dto.ts
@@ -1,9 +1,13 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
 
 export class VerifyOtpDto {
+  @IsNotEmpty()
   @IsEmail()
+  @MaxLength(50)
   email: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Matches(/^\d{4}$/, { message: 'OTP code must be exactly 4 digits' })
   otpCode: string;
 }
